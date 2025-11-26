1b1fdb89330adc77d64440b6076436cf79827883
Đỗ Đức Anh <anhdott1@gmail.com>
2025-11-26 14:55:58 +0700

    update url

--- src/hooks/useChatSocket.tsx
+++ src/hooks/useChatSocket.tsx
@@ -34,7 +34,7 @@ export function useChatSocket({
         }
 
         //socket.io thực hiện handshake → tạo kết nối WebSocket, gọi handleConnection ở backend
-        const socket = io("http://localhost:3000/chat", {
+        const socket = io("https://blogplatform-backend-2ikg.onrender.com/chat", {
           auth: { token: t },
           transports: ["websocket"],
           withCredentials: true,
